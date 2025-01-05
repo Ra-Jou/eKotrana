b69aa985d824c84ea9bfba69530242300ab2ed93
Joary <joary.andriantsoa@gmail.com>
2025-01-05 22:26:38 +0300

    add react js

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,21 @@
-import './bootstrap.js';
+// import './bootstrap.js';
 /*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
+* Welcome to your app's main JavaScript file!
+*
+* This file will be included onto the page via the importmap() Twig function,
+* which should already be in your base.html.twig.
+*/
 import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import Example from './components/Exemple.jsx';
+
+// Trouvez l'élément DOM où rendre votre composant
+const rootElement = document.getElementById('react-root');
+if (rootElement) {
+    const root = createRoot(rootElement);
+    root.render(<Example />);
+}
--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,8 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)  
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/styles/app.css')
+    .addStyleEntry('styles', './assets/styles/app.css')
+    .addEntry('app', './assets/app.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
@@ -61,7 +62,7 @@ Encore
     //.enableTypeScriptLoader()
 
     // uncomment if you use React
-    //.enableReactPreset()
+    .enableReactPreset()
 
     // uncomment to get integrity="..." attributes on your script & link tags
     // requires WebpackEncoreBundle 1.4 or higher
