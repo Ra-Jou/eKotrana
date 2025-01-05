a2b7e9de9b9c07393297d807ae5f27723f69fb98
Joary <joary.andriantsoa@gmail.com>
2025-01-05 21:23:44 +0300

    add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    './templates/**/*.html.twig', // Tous les fichiers Twig dans /templates
+    './assets/**/*.js', // Si vous utilisez des fichiers JavaScript
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
